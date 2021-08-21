9581153b686a260efaea6a447277c81c6a6beb72
megdye <79653484+megdye@users.noreply.github.com>
2021-08-21 20:26:31 +0100

    changed conflict

--- Olympick_project/Public API/all_codes/config.py
+++ Olympick_project/Public API/all_codes/config.py
@@ -1,14 +1,5 @@
-from app import app
-from flaskext.mysql import MySQL
-
 HOST = "localhost"
 USER = "root"
 PASSWORD = "Shenanigans1"
 
-# mysql = MySQL()
-#
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'Shenanigans1'
-# app.config['MYSQL_DATABASE_DB'] = 'olympick'
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# mysql.init_app(app)
+
